359840e52bd2ecbb1c532dac7fc3956bead02135
Furuikeya <furuburi@gmail.com>
2023-08-17 15:25:20 +0545

    added namaste log and fix some text on welcome page

--- src/pages/welcome/welcome.component.jsx
+++ src/pages/welcome/welcome.component.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import './welcome.styles.scss';
+import namaste from './namaste.jpg';
 
 const Welcome = () => {
   return (
-    <div className='welcome'>
-      <h1>Welcome!</h1>
+    <div className='welcome-container'>
+      <img src={namaste} alt='namaste' />
       <h4>
-        Welcome Ivan furi Shirokov's page! I am thrilled to have you here. With
-        a passion for crafting exceptional websites, I am dedicated to bringing
-        your digital visions to life. As an experienced web developer, I
-        specialize in creating responsive and user-friendly designs that not
-        only captivate but also deliver optimal functionality. Whether you need
-        a striking portfolio website, a robust e-commerce platform, or a dynamic
-        web application, I am committed to providing you with tailor-made
-        solutions that exceed your expectations. By leveraging the latest
-        technologies and industry best practices, I ensure that your online
-        presence stands out in the competitive digital landscape. Let's
-        collaborate and transform your ideas into a stunning reality. Together,
-        we can elevate your online presence and help your business thrive. Thank
-        you for visiting, and I look forward to working with you!{' '}
+        Welcome to Ivan "furi" Shirokov's page! I am thrilled to have you here.
+        With a passion for crafting exceptional websites, I am dedicated to
+        bringing your digital visions to life. As a web developer with over then
+        5 years of experience, I specialize in creating responsive and
+        user-friendly designs that not only captivate but also deliver optimal
+        functionality. Whether you need a striking portfolio website, a robust
+        e-commerce platform, or a dynamic web application, I am committed to
+        providing you with tailor-made solutions that exceed your expectations.
+        By leveraging the latest technologies and industry best practices, I
+        ensure that your online presence stands out in the competitive digital
+        landscape. Let's collaborate and transform your ideas into a stunning
+        reality. Together, we can elevate your online presence and help your
+        business thrive. Thank you for visiting, and I look forward to working
+        with you!
       </h4>
     </div>
   );
